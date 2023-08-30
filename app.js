58bfe9ff1c993e75175a243ac706ce24bbebdfe4
Mehari Fitihamlak <meharifitih@gmail.com>
2023-08-30 11:54:19 +0300

    serving static files

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 // 1) MIDDLEWARES
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware :)');
