f98cad5fc0b3a0f5ff7efb7fd7bc1d9d5f29e815
Mehari Fitihamlak <meharifitih@gmail.com>
2023-09-05 13:35:45 +0300

    adde some statu endpoint that use mongo aggregates

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 router
   .route('/top-5-cheap')
   .get(tourController.alisaTopTours, tourController.getAllTours);
