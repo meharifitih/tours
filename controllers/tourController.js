7752c7d044e370c21c25c9b6d7041c40107b652c
Mehari Fitihamlak <meharifitih@gmail.com>
2023-09-05 11:08:53 +0300

    add alise route for top 5 cheap

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,17 @@
 const Tour = require('../models/tourModel');
 
+exports.alisaTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
     // BUILD QUERY
+
     // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
